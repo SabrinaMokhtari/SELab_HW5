68a4ea95f88ea7eb8ef0375a313e18f91a3b96bd
Sabrineh <Sabrinehmokhtari@yahoo.com>
2021-04-23 17:17:33 +0430

    added swagger doc

--- Architecture_BookShop/urls.py
+++ Architecture_BookShop/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 api_patterns = [
     path('books/', include('books.urls')),
@@ -26,4 +28,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(api_patterns)),
 ]
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Books API",
+        default_version='v1',
+        description="API documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
+api_doc = [
+    path('drf-view/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
+
+urlpatterns += api_doc
--- books/views.py
+++ books/views.py
@@ -1,13 +1,24 @@
 import datetime
 
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import mixins, viewsets
 from rest_framework.response import Response
 
 from books.models import BookEntity
 from books.serializers import BookSerializers
 
+title__icontains = openapi.Parameter('title__icontains', openapi.IN_QUERY,
+                                     description="return the books with this title", type=openapi.TYPE_STRING)
+category__icontains = openapi.Parameter('category__icontains', openapi.IN_QUERY,
+                                        description="return books in this category", type=openapi.TYPE_STRING)
+created_at = openapi.Parameter('created_at', openapi.IN_QUERY, description="return all books created in this time",
+                               type=openapi.TYPE_STRING)
 
+
+@method_decorator(name='list', decorator=swagger_auto_schema(manual_parameters=[title__icontains, category__icontains, created_at]))
 class BookViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet,
                   mixins.CreateModelMixin, mixins.UpdateModelMixin):
     queryset = BookEntity.objects.all()
