465be95bdeb0792c915339f8c80d9ac8304bd958
Sabrineh <Sabrinehmokhtari@yahoo.com>
2021-04-23 14:30:30 +0430

    added initial files

---
+++ books/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class BookEntity(models.Model):
+    Registered = "Registered"
+    Preparing = "Preparing"
+    Delivering = "Delivering"
+    Received = "Received"
+    TYPE_CHOICES = (
+        (Registered, 'Registered'),
+        (Preparing, 'Preparing'),
+        (Delivering, 'Delivering'),
+        (Received, 'Received')
+    )
+    title = models.TextField()
+    Category = models.CharField(max_length=100)
+    status = models.CharField(max_length=50, choices=TYPE_CHOICES, default=Registered)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
