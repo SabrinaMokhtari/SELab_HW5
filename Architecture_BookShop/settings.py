afaf1d6b663829f07e9efbaeb1134189e9694b9e
hassan <h.zaker2222@gmail.com>
2021-04-30 19:36:28 +0430

    6

--- Architecture_BookShop/settings.py
+++ Architecture_BookShop/settings.py
@@ -78,10 +78,16 @@ WSGI_APPLICATION = 'Architecture_BookShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_db',
+        'USER': 'hassan',
+        'PASSWORD': 'hassan123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
